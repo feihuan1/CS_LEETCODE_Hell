02c887b2e61c2b5163cf11a0170c2873414a2a71
Feihuan Peng <ericrealtor1997@gmail.com>
2025-03-02 23:33:08 -0800

    asd

---
+++ 01_TwoSum/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+public class Program
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        // i hate this so much!!!
+        Dictionary<int, int> obj = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int diff = target - nums[i];
+            if (obj.ContainsKey(diff))
+            {
+                return new int[] { obj[diff], i };
+            }
+            else
+            {
+                obj[nums[i]] = i;
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+
+    static void Main()
+    {
+        int[] numbers = { 3,2,4 };
+        int target = 6;
+        int[] result = TwoSum(numbers, target);
+
+        Console.Write(string.Join(", ", result));
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+### note
+if dont need get familiar with c# syntax fast, try solve leetcode with other languages!!!!
