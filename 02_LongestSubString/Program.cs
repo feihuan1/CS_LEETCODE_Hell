8704e4ea1fd0d58a6cd4ba5e302a4eb96888114e
Feihuan Peng <ericrealtor1997@gmail.com>
2025-03-02 23:52:38 -0800

    asd

---
+++ 02_LongestSubString/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+public class Program
+{
+    public static int LengthOfLongestSubstring(string s)
+    {
+        HashSet<char> set = new HashSet<char>();
+
+        int maxLength = 0;
+        int left = 0;
+
+        for(int right = 0; right < s.Length; right++)
+        {
+            while(set.Contains(s[right]))
+            {
+                set.Remove(s[left]);
+                left++;
+            }
+
+            set.Add(s[right]);
+
+            maxLength = Math.Max(maxLength, right - left + 1);
+
+        }
+
+
+        return maxLength;
+    }
+
+    static void Main()
+    {
+        System.Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
+        System.Console.WriteLine(LengthOfLongestSubstring("bbbbb"));
+        System.Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
+    }
+}
