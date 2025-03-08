f642fa1ef568e16c35c9301fa61192ae52af46f4
Feihuan Peng <ericrealtor1997@gmail.com>
2025-03-08 15:19:04 -0800

    asd

---
+++ 049_Group_Anagram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    static List<List<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> anaGroup = new Dictionary<string, List<string>>();
+
+        foreach(string str in strs)
+        {
+            int[] charCount = new int[26];
+
+            foreach(char c in str)
+            {
+                charCount[c - 'a']++;
+            }
+
+            StringBuilder keyBuilder = new StringBuilder();
+
+            for(int i = 0; i < 26; i++)
+            {
+                keyBuilder.Append("#");
+                keyBuilder.Append(charCount[i]);// "#1#0#10..."
+            }
+
+            string key = keyBuilder.ToString();
+
+            if(!anaGroup.ContainsKey(key))
+            {
+                anaGroup[key] = new List<string>();
+            }
+
+            anaGroup[key].Add(str);
+        }
+
+
+        return new List<List<string>>(anaGroup.Values);
+    }
+
+    static void Main()
+    {
+       string[] strs  = ["eat","tea","tan","ate","nat","bat"];
+    
+        List<List<string>> res =GroupAnagrams(strs);//[["bat"],["nat","tan"],["ate","eat","tea"]]
+
+        foreach(List<string> group in res)
+        {
+            System.Console.WriteLine(string.Join(", ", group));
+        }
+    }
+}
